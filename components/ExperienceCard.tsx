dc2b1290cc36e6469794d422ef1b5b9ae6d8abea
AdamKuemmel <Adamkuemmel@gmail.com>
2022-09-30 17:43:54 -0600

    carosel

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ExperienceCard({ experience }: Props) {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-80 md:w-[600px] xl:w-[900px] snap-start p-5 mt-24 bg-slate-700/40 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-y-scroll  scrollbar-thin scrollbar-track-slate-500 scrollbar-thumb-[#edf1cf]">
+    <article className="flex flex-col rounded-lg items-center justify-center flex-shrink-0 w-80 md:w-[600px] xl:w-[900px]  p-5 mt-24 bg-slate-700/40 mx-auto">
       <motion.img
         initial={{
           y: -100,
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -2,28 +2,42 @@ import React from "react";
 import { motion } from "framer-motion";
 import ExperienceCard from "./ExperienceCard";
 import { Experience } from "../typings";
+import { Carousel } from "flowbite-react";
 type Props = {
   experiences: Experience[];
 };
 
 export default function WorkExperience({ experiences }: Props) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{
-        duration: 1.5,
-      }}
-      className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-left md:flex-row justify-evenly"
-    >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        experience
-      </h3>
-      <div className="flex w-full p-10 space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/30 scrollbar-thumb-yellow-300/80">
-        {experiences?.map((experience) => (
-          <ExperienceCard experience={experience} key={experience._id} />
-        ))}
+    <>
+      {/* <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="relative flex flex-col items-center h-screen max-w-full px-10 mx-auto overflow-hidden text-left md:flex-row justify-evenly"
+      >
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+          experience
+        </h3>
+        <div className="flex w-full p-10 space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/30 scrollbar-thumb-yellow-300/80">
+          {experiences?.map((experience) => (
+            <ExperienceCard experience={experience} key={experience._id} />
+          ))}
+        </div>
+      </motion.div> */}
+      <div className="relative z-0 flex flex-col items-center h-screen pt-20 mx-auto overflow-hidden text-left max-w-7xl md:flex-row justify-evenly">
+        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+          experience
+        </h3>
+        <Carousel>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          {experiences?.map((experience) => (
+            <ExperienceCard experience={experience} key={experience._id} />
+          ))}
+        </Carousel>
       </div>
-    </motion.div>
+    </>
   );
 }
